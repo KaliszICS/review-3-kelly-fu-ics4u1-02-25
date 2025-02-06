548dd936136511b66106eed97b0ba9faf62836ef
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-02-06 15:20:41 +0000

    Update review 3

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -18,7 +18,8 @@ public class PracticeProblem {
 		System.out.print("In: ");
 		String c;
 		c = in.nextLine();
-		System.out.print(c.StringAt(0));
+		System.out.print(c.charAt(0));
+		System.out.println();
 	}
 
 	public static void q2() {
@@ -28,22 +29,42 @@ public class PracticeProblem {
 		Boolean trueFalse;
 		trueFalse = in.nextBoolean();
 		System.out.print(!trueFalse);
-		
+		System.out.println();
 	}
 
 	public static void q3() {
 		//Write question 3 code here
-
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		int num;
+		num = in.nextInt();
+		System.out.print(num > 5);
+		System.out.println();
 		
 	}
 
 	public static void q4() {
 		//Write question 4 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		double num2;
+		num2 = in.nextDouble();
+		in.nextLine();
+		System.out.print((-2 < num2) && (num2 < 2));
+		System.out.println();
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		String c;
+		c = in.nextLine();
+		char words;
+		words = "Hello World";
+		Boolean flag;
+		flag = 
+		System.out.print()
 		
 	}
 

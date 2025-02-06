94d6a0608c88d9a3e848eeb664bfd1913e8e1bf7
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-02-06 14:57:05 +0000

    Update Review 3

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -14,16 +14,26 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		String c;
+		c = in.nextLine();
+		System.out.print(c.StringAt(0));
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		Boolean trueFalse;
+		trueFalse = in.nextBoolean();
+		System.out.print(!trueFalse);
 		
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+
 		
 	}
 
